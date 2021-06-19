325d6b9e4e0e1abf1b7ea238e076dc5f49efde3a
Mary <marygerina1204@gmail.com>
2021-06-19 13:49:31 +0300

    Initial Commit

---
+++ CMAlbumList/CMAlbumListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CMAlbumListApp.swift
+//  CMAlbumList
+//
+//  Created by Mary Gerina on 19.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct CMAlbumListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
